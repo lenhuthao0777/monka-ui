81cd731e22b705fc35fbf3ff7f8e53df448a2476
LeNhutHao <lenhuthao0777@gmail.com>
2024-01-30 20:12:10 +0700

    test husky

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,43 +1,69 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path')
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json')
 
-/*
- * This is a custom ESLint configuration for use with
- * internal (bundled by their consumer) libraries
- * that utilize React.
- *
- * This config extends the Vercel Engineering Style Guide.
- * For more information, see https://github.com/vercel/style-guide
- *
- */
-
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
-  plugins: ["only-warn"],
+  extends: ['eslint-config-turbo'],
+  plugins: ['prettier'],
+  parserOptions: {
+    project,
+  },
   globals: {
-    React: true,
     JSX: true,
   },
-  env: {
-    browser: true,
-  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    '.eslintrc.js',
+    '**/*.css',
+    '*.snap',
+    'jest.config.js',
+    '**/*.js',
+  ],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project,
       },
+      node: {
+        extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx'],
+      },
     },
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
-  ],
+  rules: {
+    'react/function-component-definition': 'off',
+    'turbo/no-undeclared-env-vars': 'off',
+    'react/jsx-key': 'off',
+    'no-dupe-class-members': 'off',
+    'prefer-arrow-callback': 'off',
+    'import/export': 'off',
+    'prettier/prettier': 'error',
+    'react/display-name': 'error',
+  },
   overrides: [
-    // Force ESLint to detect .tsx files
-    { files: ["*.js?(x)", "*.ts?(x)"] },
+    {
+      parser: '@typescript-eslint/parser',
+      files: ['**/*.ts', '**/*.tsx'],
+      plugins: ['@typescript-eslint'],
+      rules: {
+        'no-use-before-define': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-sort-props': 'off',
+        'no-void': 'off',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            caughtErrorsIgnorePattern: '^_',
+          },
+        ],
+        '@typescript-eslint/no-use-before-define': [
+          'error',
+          { functions: false, classes: false },
+        ],
+      },
+    },
   ],
-};
+}
